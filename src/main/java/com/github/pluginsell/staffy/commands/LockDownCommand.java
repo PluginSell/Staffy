57ebc6620d063e80508f585a0bd56772525aa6f4
micha <michaelofficialdeveloper@gmail.com>
2025-01-02 10:00:15 -0600

    Forgot to add a no permission message to LockDownCommand.java

--- src/main/java/com/github/pluginsell/staffy/commands/LockDownCommand.java
+++ src/main/java/com/github/pluginsell/staffy/commands/LockDownCommand.java
@@ -9,12 +9,16 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import static com.github.pluginsell.staffy.Main.prefix;
+
 public class LockDownCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(sender instanceof Player) {
             if(HashMaps.staffMode.containsKey((Player) sender)) {
                 new StaffUtils().lockDown((Player) sender);
+            } else {
+                sender.sendMessage(prefix + new MessageUtils().color("&cYou do not have permission to access that command."));
             }
         }
         return true;
